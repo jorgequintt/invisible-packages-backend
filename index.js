5e25e21d58ab485661d574e980f44158082f4bda
Jose <jorge.trbj@gmail.com>
2020-07-08 22:38:21 -0500

    Repository init

---
+++ index.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const app = require('express')();
+
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+
+db.on('error', (err) => console.log("mongoose error:", err));
+db.once('open', () => {
+	console.log("mongoose connected to db");
+})
