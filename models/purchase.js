ab06a7ee7782342ca79025e825475b4fdc133e80
Juan Gonzalez <jgu1@hotmail.com>
2020-07-11 15:24:14 -0400

    Add purchase model schema

---
+++ models/purchase.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const purchaseSchema = mongoose.Schema({
+	userId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "User",
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
+	total: Number,
+	cart: [
+		{
+			productId: {
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "Product",
+			},
+			quantity: Number,
+		},
+	],
+	location: {
+		type: {
+			type: Point,
+			required: true,
+		},
+		coordinates: {
+			type: [Number],
+			required: true,
+		},
+	},
+});
+
+module.exports = mongoose.model("Purchase", purchaseSchema);
