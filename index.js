f086b3ae40eac2ee000c8667143dfeca58ae6fb4
Jose <jorge.trbj@gmail.com>
2020-07-14 18:46:00 -0500

    editorconfig setup

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-const app = require('express')();
+require("dotenv").config();
+const mongoose = require("mongoose");
+const app = require("express")();
 
-mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.DB_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 const db = mongoose.connection;
 
-db.on('error', (err) => console.log("mongoose error:", err));
-db.once('open', () => {
+db.on("error", (err) => console.log("mongoose error:", err));
+db.once("open", () => {
 	console.log("mongoose connected to db");
-})
+});
